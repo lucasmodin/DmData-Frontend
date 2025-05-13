aa3a59af1e0c4731625d04457a4811a60cec3a00
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2025-05-13 20:51:38 +0200

    now scrolling down to sections like intro and contact info

--- js/headerANDfooter/headerANDfooter.js
+++ js/headerANDfooter/headerANDfooter.js
@@ -33,7 +33,7 @@ function loadNavLogic() {
     if (serviceLink) {
         serviceLink.addEventListener('click', function(event) {
             event.preventDefault(); // gør det muligt at selv programmere sidens adfærd
-            window.location.href = "case.html"; //hardcoded fil navn til navigation
+            window.location.href = "#intro"; //hardcoded fil navn til navigation
         });
     }
 
@@ -62,7 +62,7 @@ function loadNavLogic() {
     if (contactLink) {
         contactLink.addEventListener('click', function(event) {
             event.preventDefault();
-            window.location.href = "contact.html";
+            window.location.href = "#contact";
         });
     }
 }
